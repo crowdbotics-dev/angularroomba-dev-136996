89834bdfc3bb567638626eba12e2a98072052258
Crowdbotics <engineering@crowdbotics.com>
2024-09-12 15:55:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "angularroomba-dev-136996",
+  url: "https://angularroomba-dev-136996.botics.co",
+  api: "https://angularroomba-dev-136996.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
